ca12fcd1bb4563bdabbbb14ef6eee4c0d790e965
Damas <mlabwa1@gmail.com>
2013-04-10 19:21:50 -0400

    Supposedly working UserThread.java Re-uploaded [correct]sqlite4java.jar

--- src/UserThread.java
+++ src/UserThread.java
@@ -1,48 +1,84 @@
+/**
+ * Author: Alexander Troshchenko, Damas Mlabwa
+ * Course: CSE 4232, Spring 2013
+ * Assignment: p3
+ * Date: April 10,2013
+ */
+
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import DatabaseHelper.Database;
+
 public class UserThread extends Thread {
-    
-    BufferedReader input;
-    
-    public UserThread(){
-        
+
+    private BufferedReader input;
+    private String fileName;
+    private Database db;
+
+    /**
+     * Create UserThread object
+     * 
+     * @param filename
+     *            database file name to execute SQL queries on
+     */
+    public UserThread(String filename) {
+        fileName = filename;
+        try {
+            db = new Database(fileName);
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
     }
 
-   
-
+    /**
+     * Introduce user to how to perform SQL queries on database.
+     */
     public void startHere() {
         System.out
                 .print("To use this P2P system please enter a valid SQL query, invalid SQL queries will not be handled and the program will exit.\n");
-        System.err.print("Example of valid SQL queries: \"insert\", \"delete\".\n");
+        System.err
+                .print("Example of valid SQL queries: \"insert\", \"delete\".\n");
     }
-    
-    public void receiveQuery(){
+
+    /**
+     * Receive SQL Queries from user, push query to runQuery() for processing.
+     */
+    public void receiveQuery() {
         input = new BufferedReader(new InputStreamReader(System.in));
         boolean receivingQuery = true;
-        
-        while(receivingQuery == true){
-            
+        System.out.println("Type \"exit\" to leave query input");
+        while (receivingQuery == true) {
+
             try {
                 String SQLCommand = input.readLine();
-                
+
+                if (SQLCommand.equals("exit")) {
+                    receivingQuery = false;
+                    input.close();
+                    System.exit(0);
+                }
+
                 runQuery(SQLCommand);
             } catch (IOException e) {
-                // TODO Auto-generated catch block
+
                 e.printStackTrace();
             }
         }
     }
-    
-    public void runQuery(String command){
-        
-    }
 
-    public static void main(String[] args) {
-        UserThread test = new UserThread();
-        
-        test.startHere();
+    /**
+     * Receive SQL query from user, execute the query to the database.
+     * 
+     * @param command
+     *            SQL query from the user
+     */
+    public void runQuery(String command) {
+
+        db.executeSQL(command);
     }
 
 }
